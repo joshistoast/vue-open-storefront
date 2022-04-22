380ba4766bd149ff03a1f072decae4adf0751d28
joshwcorbett <joshwcorbett@icloud.com>
2022-04-21 18:02:57 -0600

    example env and storefront api setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
 
+  runtimeConfig: {
+    public: {
+      storefrontApiAccessToken: process.env.STOREFRONT_API_ACCESS_TOKEN,
+      storefrontApiEndpoint: process.env.STOREFRONT_API_ENDPOINT,
+    }
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
   ],
