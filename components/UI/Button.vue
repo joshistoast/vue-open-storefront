ec80a10d6a4186fcb3f5e3842143e63beae3a107
joshwcorbett <joshwcorbett@icloud.com>
2022-05-18 17:58:47 -0600

    Button UI component

---
+++ components/UI/Button.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+interface Button {
+  icon?: string
+  prependIcon?: boolean
+  appendIcon?: boolean
+  stretch?: boolean
+  disabled?: boolean
+  href?: string
+  to?: string
+  color?: 'default' | 'alternative' | 'dark' | 'light' | 'green' | 'red' | 'yellow'
+  pill?: boolean
+  size?: 'sm' | 'md' | 'lg'
+}
+const props = withDefaults(defineProps<Button>(), {
+  color: 'default',
+  size: 'md',
+})
+
+const is = props.href || props.to ? resolveComponent('NuxtLink') : 'button'
+
+</script>
+
+<template>
+  <component
+    :is="is"
+    :href="href || null"
+    :to="to || null"
+    :disabled="disabled"
+    @click="$emit('click')"
+    class="items-center justify-center font-medium border focus:ring-4 focus:outline-none"
+    :class="[
+      stretch  ? 'flex w-full'  : 'inline-flex w-auto justify-self-start',
+      pill     ? 'rounded-full' : 'rounded-lg',
+      disabled ? 'opacity-60 cursor-not-allowed' : 'cursor-pointer',
+      color === 'default'     ? 'bg-indigo-600 border-transparent text-white hover:bg-indigo-700 focus:ring-indigo-300' :
+      color === 'alternative' ? 'bg-white text-gray-900 border-gray-200 hover:bg-gray-100 hover:text-indigo-600 focus:ring-gray-200' :
+      color === 'dark'        ? 'text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-300 border-transparent' :
+      color === 'light'       ? 'bg-white text-gray-900 border-gray-200 hover:bg-gray-100 focus:ring-gray-200' :
+      color === 'green'       ? 'bg-emerald-600 text-white hover:bg-emerald-700 focus:ring-emerald-300 border-transparent' :
+      color === 'red'         ? 'bg-rose-600 text-white border-transparent focus:ring-rose-300 hover:bg-rose-700' :
+      color === 'yellow'      ? 'bg-yellow-500 text-white border-transparent focus:ring-yellow-300 hover:bg-yellow-600' : '',
+      size === 'sm'           ? 'py-2 px-3 text-sm' :
+      size === 'md'           ? 'px-5 py-2.5 text-sm' :
+      size === 'lg'           ? 'px-5 py-3 text-base': '',
+    ]"
+  >
+    <span v-if="icon && (prependIcon || (!prependIcon && !appendIcon))">
+      <Icon :icon="icon" class="w-6 h-6 mx-1" />
+    </span>
+    <span class="mx-1"><slot /></span>
+    <span v-if="icon && appendIcon && !prependIcon">
+      <Icon :icon="icon" class="w-6 h-6 mx-1" />
+    </span>
+  </component>
+</template>
