43abb4219ed56a1a2f0e46dcbc9f59196fb5778c
joshwcorbett <joshwcorbett@icloud.com>
2022-05-23 18:30:03 -0600

    central store

---
+++ stores/index.ts
@@ -0,0 +1,7 @@
+import { useCart } from './cart'
+import { useCustomer } from './customer'
+import { useFilters } from './filter'
+import { useProduct } from './product'
+import { useShop } from './shop'
+
+export { useCart, useCustomer, useFilters, useProduct, useShop }
