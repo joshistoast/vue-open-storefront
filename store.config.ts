e432e614bac15f4891530d2c717478d8b898b2f5
joshwcorbett <joshwcorbett@icloud.com>
2022-06-02 17:30:03 -0600

    add store config examples

--- store.config.ts
+++ store.config.ts
@@ -3,7 +3,9 @@ import { storeConfig } from './types'
 // Enter your store config here
 export const useStoreConfig: storeConfig = {
 
+  // example: xczxc98x7cz7cx67zx9c8zx7c6cxzic1
   storefrontApiToken: '',
-  storefrontApiUrl: ''
 
+  // example: https://my-store-url.myshopify.com/api/2022-07/graphql.json
+  storefrontApiUrl: ''
 }
