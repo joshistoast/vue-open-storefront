fa9a11f46279b28344d065945957675b4ad3a7d1
joshwcorbett <joshwcorbett@icloud.com>
2022-06-02 19:52:43 -0600

    redesigned product page media

--- components/Product/Media.vue
+++ components/Product/Media.vue
@@ -1,34 +1,39 @@
 <script setup lang="ts">
 import { MediaConnection, Media } from '@/types'
+import { useProduct } from '@/stores'
+import { v4 as uuidv4 } from 'uuid'
+
+// TODO: fix types
 
 interface ProductMedia {
   media: MediaConnection
 }
 const props = defineProps<ProductMedia>()
 const activeMedia = ref<Media>(props.media.edges[0]?.node)
+const selectedVariantImage = computed(() => useProduct().variant?.image)
 
 const setActiveMedia = (media: Media) => {
   activeMedia.value = media
 }
 const isActiveMedia = (media: Media) => activeMedia.value === media
 
+watch(selectedVariantImage, (newVariant, oldVariant) => {
+  if (newVariant !== oldVariant && !!selectedVariantImage.value) {
+    setActiveMedia({
+      __typename: 'MediaImage',
+      previewImage: selectedVariantImage.value,
+      alt: ''
+    })
+  }
+})
+
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-4">
-    <div class="overflow-hidden rounded-lg" :key="activeMedia.previewImage.id">
-      <template v-if="activeMedia.__typename === 'MediaImage'">
-        <UIShopifyImage :image="activeMedia.previewImage" />
-      </template>
-      <template v-else-if="activeMedia.__typename === 'Video'">
-        <video controls autoplay loop muted playsinline>
-          <source v-for="source in activeMedia.sources" :src="source.url" :type="`video/${source.format}`" />
-        </video>
-      </template>
-    </div>
-    <div
-      class="grid grid-cols-4 gap-4"
-    >
+  <div class="grid grid-cols-5 gap-4 place-self-start">
+
+    <!-- Media List -->
+    <div class="grid content-start order-2 grid-cols-4 col-span-5 gap-4 md:grid-cols-3 lg:col-span-1 lg:grid-cols-1 lg:order-1 place-self-start">
       <div
         v-for="mediaItem in media.edges"
         @click="!isActiveMedia(mediaItem.node) ? setActiveMedia(mediaItem.node) : false"
@@ -37,8 +42,30 @@ const isActiveMedia = (media: Media) => activeMedia.value === media
           isActiveMedia(mediaItem.node) ? 'ring-indigo-500' : 'ring-transparent hover:ring-gray-300'
         ]"
       >
-        <UIShopifyImage class="object-cover object-center w-full h-full" :image="mediaItem.node.previewImage" />
+        <UIShopifyImage
+          class="object-cover object-center w-full h-full"
+          :image="mediaItem.node.previewImage"
+        />
       </div>
+    </div>
+
+    <!-- Media Preview -->
+    <div
+      class="order-1 col-span-5 overflow-hidden rounded-lg lg:col-span-4 place-self-start lg:order-2"
+      :key="uuidv4()"
+    >
+      <template v-if="activeMedia.__typename === 'MediaImage'">
+        <UIShopifyImage :image="activeMedia.previewImage" />
+      </template>
+      <template v-else-if="activeMedia.__typename === 'Video'">
+        <video controls autoplay loop muted playsinline>
+          <source
+            v-for="source in activeMedia.sources"
+            :src="source.url"
+            :type="`video/${source.format}`"
+          />
+        </video>
+      </template>
     </div>
   </div>
 </template>
--- components/UI/ShopifyImage.vue
+++ components/UI/ShopifyImage.vue
@@ -11,16 +11,18 @@ const props = defineProps<ShopifyImage>()
 const sizes = `(max-width: ${breakpointsTailwind.md}px) 45vw, 20vw`
 const srcset = getSrcset(props.image.url)
 
+// TODO: fix error 'loading' does not exist on 'ElementAttrs<ImgHTMLAttributes>'
+
 </script>
 
 <template>
   <img
     :alt="image.altText || ''"
     :height="image.height || null"
-    loading="lazy"
     :srcset="srcset || null"
     :sizes="sizes || null"
     :width="image.width || null"
     src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+    loading="lazy"
   />
 </template>
