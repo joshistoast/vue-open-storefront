fa7e17c51fb3601eadac32420d4778fd302749af
joshwcorbett <joshwcorbett@icloud.com>
2022-06-11 19:57:57 -0600

    remove old redirect logic from account

--- pages/account.vue
+++ pages/account.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-import { useCustomer } from '@/stores'
-
 useHead({
   title: 'Account',
-})
-const router = useRouter()
-router.beforeEach((to, from) => {
-  if(to.name === 'account' && !useCustomer().isSignedIn) {
-    return navigateTo('/login')
-  }
 })
 </script>
 
