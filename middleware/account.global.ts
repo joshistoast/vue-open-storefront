bac949f745500ad826b6291baf52d4eed36a3242
joshwcorbett <joshwcorbett@icloud.com>
2022-06-11 21:58:24 -0600

    remove middleware console log

--- middleware/account.global.ts
+++ middleware/account.global.ts
@@ -2,8 +2,6 @@ import { useCustomer } from '@/stores'
 
 export default defineNuxtRouteMiddleware((to, from) => {
 
-  console.log('This is the middleware')
-
   // redirect to login if not logged in
   if (to.name === 'account' && !useCustomer().isSignedIn) {
     return navigateTo({ name: 'account-login' })
